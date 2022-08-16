fb6f23d105315c211e591d4bd5d53cc8c4d54781
Ash-tik <a.zhurikho@gmail.com>
2022-08-17 01:44:15 +0300

    task is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        console.log(min);
+        console.log(max);
     }
 
     guess() {
-
+        console.log('guess = ' + (Math.round((this.min + this.max) / 2)));
+        return (Math.round((this.min + this.max) / 2));
     }
 
     lower() {
-
+        this.max = (Math.round((this.min + this.max) / 2));
+        console.log('lowerMax' + this.max);
     }
 
     greater() {
-
+        this.min = (Math.round((this.min + this.max) / 2));
+        console.log(' greaterMin' + this.min);
     }
+
 }
 
 module.exports = GuessingGame;
